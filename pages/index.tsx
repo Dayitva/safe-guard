75bbb7f0167aaf60ae4448595be6aee36d410cdc
dayitva <dayitvagoel@gmail.com>
2022-12-04 05:42:17 +0530

    fix: gas and time lock wales left

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,12 @@ function Home() {
   const [isDenyListModalOpen, setIsDenyListModalOpen] = useState(false)
   const onDenyListModalClose = async () => {
     setIsDenyListModalOpen(false)
-  }  
+  }
+
+  const [isGasModalOpen, setIsGasModalOpen] = useState(false)
+  const onDGasModalClose = async () => {
+    setIsDenyListModalOpen(false)
+  }
 
   const [isTransactionInitatedModalOpen, setIsTransactionInitiatedModalOpen] = useState(false)
   const onTransactionInitiatedModalClose = async () => {
@@ -38,9 +43,10 @@ function Home() {
   const onClick = async (index: number) => {
     if (isConnected) {
       if (index === 0) {
-      setIsAllowListModalOpen(true)
+        setIsAllowListModalOpen(true)
       } else if (index === 1) setIsDenyListModalOpen(true)
-      else {}
+      else if (index === 2) { setIsGasModalOpen(true) }
+      else if (index === 3) setIsTransactionInitiatedModalOpen(true)
     } else {
       connect({ connector: connectors[0] })
     }
@@ -55,21 +61,21 @@ function Home() {
     </Text>
     <Flex justify={'start'} w='100%'>
       <Text mt={10} fontWeight='700' fontSize={'24px'}>
-      Guards
+        Guards
       </Text>
     </Flex>
     <Flex mt={4}>
-        {guards.map((guard, index) => {
-          return <Flex flex={1} mx={4} align='center' direction='column' key={index} borderRadius={'8px'} boxShadow='0px 0px 12px rgba(0, 0, 0, 0.1)' bg='white' p={8}>
-            <Image src={guard.icon} boxSize='72px' alt={guard.title}/>
-            <Text fontWeight={'500'} fontSize='20px' mt={6}>
-              {guard.title}
-            </Text>
-            <Text mt={1} textAlign='center'>{guard.subtitle}</Text>
-            <Button color='white' mt={6} bg='#47C95E' onClick={() => {onClick(index)}}>Set Guard</Button>
-          </Flex>
-        })}
-      </Flex>
+      {guards.map((guard, index) => {
+        return <Flex flex={1} mx={4} align='center' direction='column' key={index} borderRadius={'8px'} boxShadow='0px 0px 12px rgba(0, 0, 0, 0.1)' bg='white' p={8}>
+          <Image src={guard.icon} boxSize='72px' alt={guard.title} />
+          <Text fontWeight={'500'} fontSize='20px' mt={6}>
+            {guard.title}
+          </Text>
+          <Text mt={1} textAlign='center'>{guard.subtitle}</Text>
+          <Button color='white' mt={6} bg='#47C95E' onClick={() => { onClick(index) }}>Set Guard</Button>
+        </Flex>
+      })}
+    </Flex>
     <AllowList isOpen={isAllowListModalOpen} onClose={onAllowListModalClose} />
     <DenyList isOpen={isDenyListModalOpen} onClose={onDenyListModalClose} />
     <ConnectSafe isOpen={isConnectSafeModalOpen} onClose={onConnectSafeModalClose} />
@@ -78,10 +84,10 @@ function Home() {
 }
 
 const guards = [
-  {title: 'Allow List', subtitle: 'Setup a list of addresses that can be used as the destination in transactions.', icon: '/allow-list-icon.svg'},
-  {title: 'Deny List', subtitle: 'Setup a list of addresses that can be used as the destination in transactions.', icon: '/deny-list-icon.svg'},
-  {title: 'Gas Price', subtitle: 'Allow transactions for a specific gas price range.', icon: '/gas-icon.svg'},
-  {title: 'Ephemereal Safe', subtitle: 'Allow safes to be used within a specific  time period.', icon: '/ephemeral-safe-icon.svg'},
+  { title: 'Allow List', subtitle: 'Setup a list of addresses that can be used as the destination in transactions.', icon: '/allow-list-icon.svg' },
+  { title: 'Deny List', subtitle: 'Setup a list of addresses that can be used as the destination in transactions.', icon: '/deny-list-icon.svg' },
+  { title: 'Gas Price', subtitle: 'Allow transactions for a specific gas price range.', icon: '/gas-icon.svg' },
+  { title: 'Time lock Safe', subtitle: 'Allow safes to be used within a specific  time period.', icon: '/ephemeral-safe-icon.svg' },
 ]
 
 export default Home
---
+++ src/modals/Gas.tsx
@@ -0,0 +1,116 @@
+import {
+    Text,
+    Flex,
+    Modal,
+    ModalCloseButton,
+    ModalContent,
+    ModalOverlay,
+    Button,
+    Image,
+    Input,
+    CircularProgress
+} from "@chakra-ui/react";
+import EthersAdapter from "@safe-global/safe-ethers-lib";
+import { useEffect, useMemo, useState } from "react";
+import { useAccount, useConnect, useContract, useSigner } from 'wagmi'
+import AllowDeployerABI from "../abis/AllowDeployer.json"
+import AllowTransactionGuardABI from "../abis/AllowTransactionGuard.json"
+import SafeServiceClient from '@safe-global/safe-service-client'
+import { delay } from "../utils/time";
+import { ethers } from "ethers"
+import Safe from "@safe-global/safe-core-sdk";
+import { ALLOW_GUARD_CONTRACT } from "../utils/constants";
+
+interface Props {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+function Gas({ isOpen, onClose }: Props) {
+    // By default 1 address needs to be there
+    const [addresses, setAddresses] = useState<string[]>([''])
+    const [safeAddress, setSafeAddress] = useState<string>()
+    const [maxGasPrice, setMaxGasPrice] = useState<string>('0.0')
+    const [loading, setLoading] = useState(false)
+    const { address } = useAccount()
+    const { data: signer } = useSigner({
+        chainId: 5,
+    })
+    // const factoryContract = useContract({
+    //     address: ALLOW_GUARD_CONTRACT,
+    //     // address: '0x0AaFbF1D44bF18e3525c894B7977164e8872f13a',
+    //     // abi: AllowDeployerABI,
+    //     abi: AllowDeployerABI,
+    //     signerOrProvider: signer
+    // })
+
+    const onContinue = async () => {
+        
+    }
+
+    return (
+        <Modal isCentered={true} isOpen={isOpen} size="2xl" onClose={onClose}>
+            <ModalOverlay backdropFilter="blur(12px)" />
+            <ModalContent>
+                <ModalCloseButton />
+                <Flex direction="column" align='center' p={6}>
+                    <Image src='/allow-list-icon.svg' w='97px' h='107px' alt='safe-header' />
+                    <Text mt={6} fontWeight='500' fontSize={'24px'}>
+                        Allow list Guard
+                    </Text>
+                    <Text fontSize='14px' lineHeight={'20px'} textAlign={'center'} mt={1}>
+                        Setup a list of addresses that can be used as the destination in transaction.
+                    </Text>
+
+                    <Flex justify={'start'} w='100%' mt={8}>
+                        <Text fontSize='14px' fontWeight={'500'} lineHeight={'20px'}>
+                            Safe address
+                        </Text>
+                    </Flex>
+
+                    <Input mt={4} bg='#E8F0F1' placeholder="0x2F05BFDc43e1bAAebdc3D507785fb942eE5c" borderRadius={'4px'} key={safeAddress} value={safeAddress} onChange={(e) => {
+                        setSafeAddress(e.target.value)
+                    }} />
+
+                    <Flex justify={'start'} w='100%' mt={8}>
+                        <Text fontSize='14px' lineHeight={'20px'}>
+                            Maximum gas cost (in ETH)
+                        </Text>
+                    </Flex>
+
+                    <Input mt={2} bg='#E8F0F1' placeholder="0.05 ETH" borderRadius={'4px'} key={safeAddress} value={safeAddress} onChange={(e) => {
+                        setSafeAddress(e.target.value)
+                    }} />
+
+                    <Flex justify={'start'} w='100%' mt={4}>
+                        <Text fontSize='14px' fontWeight={'500'} lineHeight={'20px'}>
+                            Address
+                        </Text>
+                    </Flex>
+
+                    {
+                        addresses.map((address, index) => {
+                            return <Input mt={2} bg='#E8F0F1' placeholder="0x2F05BFDc43e1bAAebdc3D507785fb942eE5c" borderRadius={'4px'} key={address} value={address} onChange={(e) => {
+                                const copy = [...addresses]
+                                copy[index] = e.target.value
+                                setAddresses(copy)
+                            }} />
+                        })
+                    }
+
+                    <Flex justify={'start'} w='100%'>
+                        <Button disabled={addresses[addresses.length - 1] === ''} color='black' leftIcon={<Image alt='address-add' src='/add.svg' boxSize={'20px'} />} p={4} variant='link' onClick={() => {
+                            setAddresses([...addresses, ''])
+                        }}>Add Address</Button>
+                    </Flex>
+
+                    <Button mt={4} p={loading ? 2 : 0} w='100%' bg='#47C95E' color='white' onClick={onContinue}>
+                        {loading ? <CircularProgress color='white' isIndeterminate /> : 'Continue'}
+                    </Button>
+                </Flex>
+            </ModalContent>
+        </Modal>
+    );
+}
+
+export default Gas;
